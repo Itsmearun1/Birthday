fb7d3f7897c0e0acf6a4c67681422333549f55f8
itsmearun1 <arundevvb1@gmail.com>
2024-04-18 19:14:29 +0530

    final

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,51 +3,21 @@ import { Fade } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 import './Carousel.css'
 const fadeImages = [
-  {
-    url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/IMG-20230104-WA0016.jpg',
-    caption: 'First Slide'
-  },
-  {
-    url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/IMG-20230122-WA0028.jpg',
-    caption: 'Second Slide'
-  },
-  {
-    url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/IMG-20230828-WA0020.jpg',
-    caption: 'Third Slide'
-  },
-  {
-    url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/IMG-20230903-WA0148.jpg',
-    caption: 'Fourth Slide'
-  },
-  {
-    url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/IMG-20231224-WA0050.jpg',
-    caption: 'Fifth Slide'
-  },
-  {
-    url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/IMG_20230908_234953.jpg',
-    caption: 'Sixth Slide'
-  },
-  {
-    url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/IMG_20240113_143324.jpg',
-    caption: 'Seventh Slide'
-  },
-  {
-    url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/IMG_20240113_180729.jpg',
-    caption: 'Eighth Slide'
-  },
-  {
-    url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/img1.jpg',
-    caption: 'Ninth Slide'
-  },
-  {
-    url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/img2.jpg',
-    caption: 'Tenth Slide'
-  },
-  {
-    url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/img3.jpg',
-    caption: 'Eleventh Slide'
-  }
+  { url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/1.jpg', caption: 'First Slide' },
+  { url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/2.jpg', caption: 'Second Slide' },
+  { url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/3.jpg', caption: 'Third Slide' },
+  { url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/4.jpg', caption: 'Fourth Slide' },
+  { url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/5.jpg', caption: 'Fifth Slide' },
+  { url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/6.jpg', caption: 'Sixth Slide' },
+  { url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/7.jpg', caption: 'Seventh Slide' },
+  { url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/8.jpg', caption: 'Eighth Slide' },
+  { url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/9.jpg', caption: 'Ninth Slide' },
+  { url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/10.jpg', caption: 'Tenth Slide' },
+  { url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/11.jpg', caption: 'Eleventh Slide' },
+  { url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/12.jpg', caption: 'Twelfth Slide' },
+  { url: 'https://raw.githubusercontent.com/Itsmearun1/Birthday/main/src/assets/13.jpg', caption: 'Thirteenth Slide' }
 ];
+
 
 
 const Slideshow = () => {
