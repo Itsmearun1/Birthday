c880c8068624792474cb5120341be008eb92b2eb
itsmearun1 <arundevvb1@gmail.com>
2024-04-18 19:32:03 +0530

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {  Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import MainPage from './components/MainPage';
 
 function App() {
   return (
-    <Router>
+   
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/main" element={<MainPage />} />
       </Routes>
-    </Router>
+    
   );
 }
 
